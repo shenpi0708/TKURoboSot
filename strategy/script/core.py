c2eb16073db6ffdfb7fe75898b72d7bb2dee5f1e
robot <shenpi0708@gmail.com>
2020-07-24 11:11:04 +0800

    test11

--- strategy/script/core.py
+++ strategy/script/core.py
@@ -49,9 +49,7 @@ class Strategy(object):
               self.robot.toSupporter()
         if self.robot.is_supporter:
           if  shootcheck:
-            print("1")
             if ball_pass_finsh:
-              print("2")
               if not otherrobot['state'] =="Chase":
                 self.robot.toChase()
 
