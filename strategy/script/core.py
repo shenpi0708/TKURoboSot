33ea415a0406f1bb3377cb33325a5dc37e75b9d4
robot <shenpi0708@gmail.com>
2020-08-31 15:43:10 +0800

    asd

--- strategy/script/core.py
+++ strategy/script/core.py
@@ -36,7 +36,7 @@ class Strategy(object):
         canpassball=True
       else :
         canpassball=False
-      if self.robot.CheckBallHandl:  
+      if self.robot.CheckBallHandle():  
         self.robot.PassRequestPass =   self.robot.AC.ball_pass_check(t[self.robot.opp_side]['dis'],\
                                        t[self.robot.opp_side]['ang'],\
                                        l['ranges'],\
